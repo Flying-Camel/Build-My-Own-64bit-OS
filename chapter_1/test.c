6619a049525afdbd950c425f09c0c33b23e4361c
tanagy <juneun0303@naver.com>
2020-04-06 22:16:37 +0900

    Init

---
+++ chapter_1/test.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(int argc, char** arvg){
+	printf("Heelo, world!!\n");
+	return 0;
+}
---
+++ platform-tools/lib/gcc/x86_64-pc-linux/8.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --target=x86_64-pc-linux --prefix=/home/leekyuhyuk/64bit/Chapter-1/src/out/tools/usr --disable-nls --enable-languages=c --without-headers --disable-shared --enable-multilib --with-system-zlib --with-gmp=/home/leekyuhyuk/64bit/Chapter-1/src/out/tools/usr --with-mpfr=/home/leekyuhyuk/64bit/Chapter-1/src/out/tools/usr --with-mpc=/home/leekyuhyuk/64bit/Chapter-1/src/out/tools/usr --with-pkgversion='Simple x86_64 Operating System' --with-bugurl=https://github.com/LeeKyuHyuk/Simple-x86_64-Operating-System/issues";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
